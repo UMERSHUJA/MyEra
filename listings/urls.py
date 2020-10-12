0ef8c12c41ae33c62fbac7bf5acd4fd8c857a8ba
UMERSHUJA <umershuja12@gmail.com>
2020-10-12 14:17:34 +0500

    template revision-3

--- listings/urls.py
+++ listings/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
     path('login', views.login, name='login'),
-    path('dashboard', views.dashboard, name='dashboard')
+    path('dashboard', views.dashboard, name='dashboard'),
+    
 ]
 
