c3aa1ef22227ddfa6b60da962890360c0010a283
UMERSHUJA <umershuja12@gmail.com>
2020-10-16 17:45:53 +0500

    update and delete startups

--- startup/urls.py
+++ startup/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.startup, name='startup'),
-    path('edit/<context>', views.editstartup, name='editstartup'),    
+    
+    path('delete/<int:startuplist_id>', views.delstartup, name='delstartup'),
+    
+    path('edit/<int:startuplist_id>', views.editstartup, name='editstartup'),
+    path('changeoccur/<int:startuplist_id>', views.changeoccur, name='changeoccur')
+
+
 ]
 
--- startup/views.py
+++ startup/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Startup
 from django.contrib import messages
 
@@ -14,10 +14,12 @@ def startup(request):
         price = request.POST['price']
         facebook = request.POST['facebook']
         twitter = request.POST['twitter']
-        if request.POST['sponsered']:
-            sponsered = True
-        else:
-            sponsered = False
+        sponsered = 'sponsered' in request.POST
+        # sponsered = request.POST['sponsered']
+        # if request.POST['sponsered']:
+        #     sponsered = True
+        # else:
+        #     sponsered = False
         
         startup = Startup(product_name=product_name, description=description, image=image, prices=price, facebook=facebook, twitter=twitter, is_sponsered=sponsered, user_id=user_id)
         
@@ -28,6 +30,34 @@ def startup(request):
         return redirect('dashboard')
 
 
+def editstartup(request, startuplist_id):
+    startups = Startup.objects.all().filter(id=startuplist_id)
+            
+    context= {
+        'startups' : startups,
+    }
+    return render(request, 'pages/editstartup.html', context)
 
-def editstartup(request):
-    return render(request, 'pages/editstartup.html')
+
+def changeoccur(request, startuplist_id):
+    if request.method == 'POST':
+        startup = Startup.objects.get(pk = startuplist_id)
+        startup.product_name = request.POST['product_name']
+        startup.description = request.POST['description']
+        startup.image = request.POST['image']
+        startup.prices = request.POST['price']
+        startup.facebook = request.POST['facebook']
+        startup.twitter = request.POST['twitter']
+        startup.is_sponsered = 'sponsered' in request.POST
+        startup.save()
+        messages.success(request, 'You startup has been altered sucessfully!')
+        
+        return redirect('dashboard')
+
+
+
+def delstartup(request, startuplist_id):
+    jobs = get_object_or_404(Startup, pk=startuplist_id)
+    jobs.delete()
+    messages.success(request, 'Your startup has been deleted successfully')
+    return redirect('dashboard')
